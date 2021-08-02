f415059e2cc72c3655cff1c1c06b437dce0a2915
Gani <ganiiz@mail.ru>
2021-08-03 00:39:16 +0600

    Initial commit

---
+++ src/main/java/ru/geekbrains/HelloServlet.java
@@ -0,0 +1,35 @@
+package ru.geekbrains;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/")
+public class HelloServlet implements Servlet {
+    private ServletConfig config;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        this.config = config;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return this.config;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.getWriter().println("<h1>Hello world!!!</h1>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
